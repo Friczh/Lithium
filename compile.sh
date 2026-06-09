6fc7fecf3451703680f97781aed087292f6479d0
Friczh <111292694+Friczh@users.noreply.github.com>
2026-06-09 16:31:09 +0700

    compile.sh - Added gperf deps

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@ echo "=== Job 2: Compile started: $(date) ==="
 
 # Install base deps
 sudo apt-get update -qq
-sudo apt-get install -qq lsb-release file git curl python3 python3-pillow
+sudo apt-get install -qq lsb-release file git curl python3 python3-pillow gperf
 
 # depot_tools - self-heal 
 if [ ! -f "$DEPOT_TOOLS/gclient" ]; then
